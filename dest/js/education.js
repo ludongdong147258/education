b04af0562b09582165974a79fb86073d3a35818c
ludongdong <ludongdong147258@163.com>
2016-09-03 13:39:18 +0800

    打包

--- dest/js/education.js
+++ dest/js/education.js
@@ -295,7 +295,7 @@ angular.module('education', ['ngResource', 'ionic', 'ngFileUpload', 'monospaced.
             $rootScope.user = JSON.parse(CONFIG.user);
         }
     }]).constant('CONFIG', {
-        urlPrefix: 'http://101.200.131.30:8020',
+        urlPrefix: location.protocol + '//' + location.host,
         // urlPrefix:'',
         token: localStorage.getItem('token'),
         user: localStorage.getItem('user'),
@@ -329,7 +329,7 @@ angular.module('education', ['ngResource', 'ionic', 'ngFileUpload', 'monospaced.
                 var reg = /^[0-9a-zA-Z\u4e00-\u9fa5]{5,30}$/;
                 return reg.test(address);
             },
-            checkName:function(name){
+            checkName: function(name) {
                 var reg = /^[\u4e00-\u9fa5]{2,7}$/;
                 return reg.test(name);
             }
@@ -338,9 +338,22 @@ angular.module('education', ['ngResource', 'ionic', 'ngFileUpload', 'monospaced.
     });
 
 angular.module('education')
-    .filter('TypeFilter', ['CONFIG', function() {
+    .filter('TypeFilter', [function() {
         return function(type) {
-            return CONFIG[type];
+            switch (type) {
+                case 'student':
+                    return '学生'
+                    break;
+                case 'teacher':
+                    return '老师'
+                    break;
+                case 'institution':
+                    return '机构'
+                    break;
+                case 'manage':
+                    return '工作人员'
+                    break;
+            }
         };
     }]);
 
